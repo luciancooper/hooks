04bbd72ab6c7c37ca380089a03de52af5d925b0e
Lucian Cooper <cooperlucian@gmail.com>
2020-12-10 14:56:57 -0500

    build: use `rollup` to bundle package in cjs & esm formats

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+    ],
+    external: ['react'],
+};
